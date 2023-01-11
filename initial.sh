3ee99ffbd86808d520d13df446553cc7f00da9c0
yazilimperver <yazilimperver@gmail.com>
2023-01-11 23:11:54 +0300

    betikler guncellenmistir

---
+++ initial.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+sudo apt update -y;
+sudo apt install git -y
+git config --global user.email yazilimperver@gmail.com
+git config --global user.name yazilimperver
+git clone git@github.com:yazilimperver/dev-setup.git
+
+sudo apt install snapd
+
+# Terminal eklentileri
+sudo apt install zsh
+chsh -s /usr/bin/zsh
+
+# oh my zsh'i kur
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install the powerline font
+sudo apt-get install fonts-powerline
+
+# Diger fontlar
+curl -LJO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -LJO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -LJO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -LJO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+sudo mkdir -p ${conf_home}/.local/share/fonts;
+sudo mv *.ttf ${conf_home}/.local/share/fonts;
+
+# Install powerlevel10k for Oh my zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Download zsh-autosuggestions :
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Download zsh-syntax-higlighting 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Install fzf-tab 
+git clone https://github.com/Aloxaf/fzf-tab ~/somewhere
+source ~/somewhere/fzf-tab.plugin.zsh
+
+# Install lsd
+sudo snap install lsd
+
+# zsh-completions
+git clone https://github.com/zsh-users/zsh-completions.git
+fpath=(path/to/zsh-completions/src $fpath)
+rm -f ~/.zcompdump; compinit
+
+# zsh config icerisinde plugins=(... sudo) ile birlikte iki esc ile sudo otomatik ekleniyor
+
+sudo apt-get install dconf-cli
+
+# Ayarlar icin referans siteler
+# https://medium.com/@satriajanaka09/setup-zsh-oh-my-zsh-powerlevel10k-on-ubuntu-20-04-c4a4052508fd
+# ZSH Konfigurasyonu icin https://linuxhint.com/configure-setup-zshrc-zsh/
+
+# `p10k configure` ile ayarlama yapabiliriz
